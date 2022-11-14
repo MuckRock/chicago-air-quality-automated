fbcdc38cf649ab1cf52668357d75d631429f622d
Dillon Bergin <news.dillonbergin@gmail.com>
2022-11-14 14:18:13 -0500

    import zoneinfo

--- etl/get_and_clean_data.py
+++ etl/get_and_clean_data.py
@@ -1,8 +1,8 @@
 import requests
 import json
 import pandas as pd
-from datetime import date, timedelta, datetime
-import time
+from datetime import date, timedelta, datetime, timezone
+from zoneinfo import ZoneInfo
 import os 
 
 
