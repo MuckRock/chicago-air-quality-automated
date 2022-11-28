f2fd2841dfec6d4b777cca68593dfe87d3375fcc
Dillon Bergin <news.dillonbergin@gmail.com>
2022-11-28 16:58:38 -0500

    re-index data pull

--- etl/get_and_clean_data.py
+++ etl/get_and_clean_data.py
@@ -41,69 +41,46 @@ df_complete = pd.concat([df_complete, readings])
 ### AGGREGATE MINUTE-BY-MINUTE DATA UP TO DAILY AVERAGES ###
 hourly_all = (
     df_complete
-    .groupby(['msrDeviceNbr', pd.Grouper(key='readingDateTimeLocal', freq="H")])
-    .agg({'readingDateTimeUTC':['nunique'], 'calibratedPM25':['mean']})
+    .groupby(['deviceFriendlyName', pd.Grouper(key='readingDateTimeLocal', freq="H")])
+    .agg({'readingDateTimeUTC':['nunique'], 'calibratedPM25':['mean'], 'latitude':['median'], 'longitude':['median']})
     .reset_index(col_level=1)
 )
 
 hourly_all.columns = hourly_all.columns.get_level_values(1)
 
+hourly_all.columns = ['device_friendly_name', 'date_time', 'nbr_of_readings', 'pm_25', 'latitude', 'longitude']
+
 # apply date completeness criteria of 75% of total possible readings in one hour
 hourly_clean = (
     hourly_all
-    .rename(columns={'msrDeviceNbr': 'msr_device_nbr', 'readingDateTimeLocal': 'date_time', 'nunique': 'nbr_of_readings', 'mean': 'pm_25'})
-    .query('nbr_of_readings > 9')
+    .query('nbr_of_readings >= 9')
 )
 
 daily_all = (
     hourly_clean
-    .groupby(['msr_device_nbr', pd.Grouper(key='date_time', freq="D")])
-    .agg({'pm_25':['mean'], 'nbr_of_readings':['sum']})
+    .groupby(['device_friendly_name', pd.Grouper(key='date_time', freq="D")])
+    .agg({'nbr_of_readings':['sum'], 'pm_25':['mean'], 'latitude':['median'], 'longitude':['median']})
     .reset_index(col_level=1)
 )
 
 daily_all.columns = daily_all.columns.get_level_values(1)
+daily_all.columns = ['device_friendly_name', 'date_time', 'nbr_of_readings', 'pm_25', 'latitude', 'longitude']
 
 # apply date completeness criteria of 75% of total possible readings in one day 
 daily_clean = (
     daily_all
-    .rename(columns={'mean':'pm_25','sum':'nbr_of_readings'})
-    .query('nbr_of_readings > 216')
-    .drop(['nbr_of_readings'], axis = 1)
-    .set_index('msr_device_nbr')
-)
-
-
-### GET DEVICE LIST FOR CANNONICAL LAT/LONGS ###
-# lat/longs from individual readings occassionally drift slightly, and we want one cannonical lat/long for each device 
-list_response = requests.get(device_list_url + f"?city={city}", headers={'ApiKey':api_key})
-device_list = list_response.json()
-device_list_df = pd.DataFrame(device_list)
-device_list_df = (
-    device_list_df[['msrDeviceNbr', 'deviceFriendlyName', 'deploymentStartDateTime', 'latitude', 'longitude', 'miscAnnotation']]
-    .rename(columns={'msrDeviceNbr': 'msr_device_nbr','deviceFriendlyName':'device_friendly_name','deploymentStartDateTime': 'time_stamp', 'miscAnnotation': 'misc_annotation'})
-)
-
-device_list_df['time_stamp'] = pd.to_datetime(device_list_df['time_stamp'])
-
-updated_list = device_list_df.sort_values(by=['time_stamp']).drop_duplicates(['msr_device_nbr'], keep='last')
-
-### JOIN CANNONICAL LAT/LONGS TO MOST RECENT DATA ###
-update_complete = pd.merge(daily_clean, updated_list, on='msr_device_nbr', how='left')
-update_complete = (
-    update_complete
-    .set_index('msr_device_nbr')
-    .drop(['time_stamp'], axis = 1)
+    .query('nbr_of_readings >= 216')
+    .set_index('device_friendly_name')
 )
 
 ### WRITE UPDATED DATA TO NEW CSV FOR EACH WEEK
 today = date.today()
 if date.today().weekday() == 1:
     output_path='data/readings/daily_updates/' +'week_start_' + str(datetime.now().strftime('%Y-%m-%d')) + '.csv'
-    update_complete.to_csv(output_path)
+    daily_clean.to_csv(output_path)
 else:
     output_path='data/readings/daily_updates/' + 'week_start_' + str((today - timedelta(days=today.weekday())).strftime('%Y-%m-%d')) + '.csv'
-    update_complete.to_csv(output_path, mode='a', header=False)
+    daily_clean.to_csv(output_path, mode='a', header=False)
 
 
 
